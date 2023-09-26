f5f7b900a922b57506462ff7fe0d8c058143a0fe
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-09-26 10:45:49 +0200

    Funciona la lista

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const taskInput = document.getElementById("taskInput");
-    const addTaskButton = document.getElementById("addTaskButton");
-    const taskList = document.getElementById("taskList");
+  const taskInput = document.getElementById("taskInput");
+  const addTaskButton = document.getElementById("addTaskButton");
+  const taskList = document.getElementById("taskList");
 
-    // Función para añadir una nueva tarea
-    function addTask() {
-        const taskText = taskInput.value.trim();
-        if (taskText !== "") {
-            const newTask = document.createElement("li");
-            newTask.textContent = taskText;
-            newTask.addEventListener("click", function () {
-                // Borrar la tarea al hacer clic en ella
-                this.remove();
-            });
-            taskList.appendChild(newTask);
-            taskInput.value = "";
-        }
+  // Función para añadir una nueva tarea
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+      const newTask = document.createElement("li");
+      newTask.innerHTML = `
+            <span>${taskText}</span>
+            <button class="deleteButton btn btn-danger">Borrar</button>
+            `;
+     
+      taskList.appendChild(newTask);
+      taskInput.value = "";
+      const deleteButton = newTask.querySelector(".deleteButton");
+      deleteButton.addEventListener("click", function(){
+        taskList.removeChild(newTask);
+      });
     }
+  }
 
-    // Escuchar el evento click en el botón "Añadir"
-    addTaskButton.addEventListener("click", addTask);
+  // Escuchar el evento click en el botón "Añadir"
+  addTaskButton.addEventListener("click", addTask);
 
-    // Escuchar el evento "Enter" en el campo de entrada
-    taskInput.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            addTask();
-        }
-    });
+  // Escuchar el evento "Enter" en el campo de entrada
+  taskInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
 });
